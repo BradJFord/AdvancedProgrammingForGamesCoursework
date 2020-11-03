ad30aa9727a3c72b1a73569b5c8db999e942e914
TheSingular1ty <49131908+TheSingular1ty@users.noreply.github.com>
2020-11-03 01:44:25 +0000

    Implementing Corridor Creation
    needs bugfixing

--- CSC8501 Maze Generation Coursework/MazeGenerator.cpp
+++ CSC8501 Maze Generation Coursework/MazeGenerator.cpp
@@ -4,7 +4,7 @@
 #include "MazeGenerator.h"
 
 
-void MazeGenerator::printMaze(vector< vector<char>> map) {
+void MazeGenerator::printMaze() {
 	for (int i = 0; i < map.size(); i++) {
 		for (int j = 0; j < map[0].size(); j++) {
 			cout << map[i][j] << " ";
@@ -14,15 +14,145 @@ void MazeGenerator::printMaze(vector< vector<char>> map) {
 		}
 	}
 }
-vector<vector<char>> MazeGenerator::carveMaze(vector<vector<char>>map, int numExits) {
+
+vector<int> MazeGenerator::createVerticalWall(int row, int point) {
+	vector<int> path;
+	cout << "Row: " << row << " Point: " << point << endl;
+	map[row][point] = 'x';
+	path.push_back(point);
+	if (row <= 1) {
+		cout << "down" << endl;
+		for (int i = row; i < map[row].size() - 1; i++) {
+			if (map[row + 1][point] == 'x') {
+				map[row][point] = 'x';
+				path.push_back(row);
+				break;
+			}
+			else {
+				map[row++][point] = 'x';
+				path.push_back(row);
+			}
+		}
+
+	}
+	else if (row <= 18) {
+		cout << "up" << endl;
+		for (int i = row; i >=1 ; i--) {
+			if (map[row - 1][point] == 'x') {
+				map[row][point] = 'x';
+				path.push_back(row);
+				break;
+			}
+			else {
+				map[row--][point] = 'x';
+				path.push_back(row);
+			}
+		}
+	}
+	int pathSize = path.size() - 1;
+	
+	if (pathSize > 1) {
+		path.erase(path.begin() + (rand() % pathSize));
+		map[path.at(rand() % pathSize)][point] = ' ';
+	}
+	return path;
+}
+vector<int> MazeGenerator::createHorizontalWall(int row, int point) {
+	vector<int> path;
+	cout << "Row: " << row << " Point: " << point << endl;
+
+	map[row][point] = 'x';
+	path.push_back(point);
+	if (point <= 1) {
+		cout << "right" << endl;
+		for (int i = point; i < map[row].size() - 1; i++) {
+			if (map[row][point + 1] == 'x') {
+				
+				map[row][point] = 'x';
+				path.push_back(point);
+				break;
+			}
+			else {
+				map[row][point++] = 'x';
+				path.push_back(point);
+			}
+		}
+	}
+	else if (point <= 18) {
+		cout << "left" << endl;
+		for (int i = point; i >= 1; i--) {
+			if (map[row][point - 1] == 'x') {
+				
+				map[row][point] = 'x';
+				path.push_back(point);
+				break;
+			}
+			else {
+				map[row][point--] = 'x';
+				path.push_back(point);
+			}
+		}
+	}
+	int pathSize = path.size() - 1;
+	
+	if (pathSize>1) {
+		path.erase(path.begin() + (rand() % pathSize));
+		map[row][path.at(rand() % pathSize)] = ' ';
+	}
+	return path;
+}
+void MazeGenerator::createCorridors(int bisectRow, int bisectPoint, int stop) {
+	int row = bisectRow;
+	int point = bisectPoint;
+
+	//choose direction to bisect the map, based on the position of the bisection point
+	cout << "Horizontal or Vert " <<horizontalOrVert << endl;
+	vector<int> path;
+
+
+	if (horizontalOrVert == 0) {
+		path = createHorizontalWall(row, point);
+		horizontalOrVert = 1;
+	}
+	else {
+		path = createVerticalWall(row,point);
+		horizontalOrVert = 0;
+	}
+	printMaze();
+	
+
+
+	cout << "path size: " << path.size() << endl;
+	int randomRowPoint = path.size();
+	randomRowPoint = rand() % randomRowPoint +1;
+	//randomRowPoint = (randomRowPoint > 0) ? randomRowPoint : rand() % (mapSize - 2);
+	stop--;
+
+		if (horizontalOrVert == 0) {
+			if (stop > 0) {
+				createCorridors(randomRowPoint, bisectPoint, stop);
+			}
+		}
+		else {
+			if (stop > 0) {
+				createCorridors(bisectRow, randomRowPoint, stop);
+			}
+		}
+		//get random point on made line make a pathway, find another random point and pass it into the next recursive call
+
+}
+
+
+//rename to generate maze and call createCorridors at the start
+void MazeGenerator::createMaze() {
 	//carve the central 3x3 starting room
 	for (int i = -2; i < 1; i++) {
 		for (int j = -2; j < 1; j++) {
-			map[(map.size() / 2) + i][(map[0].size() / 2) + j] = ' ';
+			map[(mapSize / 2) + i][(mapSize / 2) + j] = ' ';
 		}
 	}
 
-	map[(map.size() / 2) - 1][(map[0].size() / 2) - 1] = 'S';
+	map[(mapSize / 2) - 1][(mapSize / 2) - 1] = 'S';
 
 	//assign exits
 	for (int i = 0; i < numExits;i++) {
@@ -49,11 +179,6 @@ vector<vector<char>> MazeGenerator::carveMaze(vector<vector<char>>map, int numEx
 		map[x][y] = 'E';
 	}
 
-	
-
-
-	return map;
-
 }
 int main() {
 	srand(time(NULL));
@@ -63,10 +188,35 @@ int main() {
 	cin >> mapSize;
 	cout << "Please the amount of exits you would like." << endl;
 	cin >> numExits;
-	vector< vector<char>> map(mapSize, vector<char>(mapSize, 'x'));
-	MazeGenerator maze;
-	//maze.printMaze(map);
-	map = maze.carveMaze(map,numExits);
-	maze.printMaze(map);
+	MazeGenerator maze(mapSize,numExits);
+	maze.horizontalOrVert = rand() % 2; //0 horizontal, 1 vertical
+
+	//choose row and point to begin the bisection of the map
+	int mapSize1 = maze.map.size() - 2;
+	int bisectRow = (rand() % mapSize1) + 1;
+	cout << " Row " << bisectRow << endl;
+	int bisectPoint = ((rand() % 2) > 0) ? mapSize-2 : 1;
+	//int bisectPoint = (rand() % mapSize1) + 1;
+	cout << "Point " << bisectPoint << endl;
+
+
+	//create outer wall of maze
+	for (int i = 0; i < (mapSize - 1); i++) {
+		maze.map[i][0] = 'x';
+	}
+	for (int i = 0; i < (mapSize - 1); i++) {
+		maze.map[0][i] = 'x';
+	}
+	for (int i = 0; i < (mapSize - 1); i++) {
+		maze.map[i][mapSize-1] = 'x';
+	}
+	for (int i = 0; i < (mapSize); i++) {
+		maze.map[mapSize-1][i] = 'x';
+	}
+	maze.printMaze();
+
+	maze.createCorridors(bisectRow, bisectPoint,30);
+	//map = maze.createCorridors(map, bisectRow, bisectPoint);
+	maze.printMaze();
 	return 0;
 }
--- CSC8501 Maze Generation Coursework/MazeGenerator.h
+++ CSC8501 Maze Generation Coursework/MazeGenerator.h
@@ -8,13 +8,28 @@ using namespace std;
 class MazeGenerator
 {
 public:
+	MazeGenerator(int mapSize, int numExits) {
+		map = vector<vector<char>>(mapSize, vector<char>(mapSize, ' '));
+		this->mapSize = mapSize;
+		this->numExits = numExits;
+	}
+
+	void printMaze();
+	void createMaze();
+	void createCorridors(int bisectRow, int bisectPoint, int stop);
+	vector<int> createVerticalWall(int row, int point);
+	vector<int> createHorizontalWall(int row, int point);
+	
 	enum Direction {
 		UP = 0,
 		DOWN = 1,
 		LEFT = 2,
 		RIGHT = 3
 	};
-	void printMaze(vector< vector<char>> map);
-	vector<vector<char>> carveMaze(vector<vector<char>> map , int numExits);
+
+	vector< vector<char>> map;
+	int mapSize;
+	int numExits;
+	int horizontalOrVert;
 };
 
